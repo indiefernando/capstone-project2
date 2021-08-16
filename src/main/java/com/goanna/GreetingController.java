802e4c792271d97df296832e8cbaeb65a423e51c
indiefernando <indie.fernando@Outlook.com>
2021-08-16 10:45:55 +1000

    Creating my variables in my Greeting class, such as gender, weight, standardDrinks and hoursDrinking

--- src/main/java/com/goanna/GreetingController.java
+++ src/main/java/com/goanna/GreetingController.java
@@ -2,4 +2,9 @@ package com.goanna;
 
 public class GreetingController {
 
+    private String gender;
+    private double weight;
+    private double standardDrinks;
+    private double hoursDrinking;
+
 }
