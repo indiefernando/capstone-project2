5c92a24e0764dfe0c68ab1fe1ba96de462d7d02a
indiefernando <indie.fernando@Outlook.com>
2021-08-16 10:04:29 +1000

    Initial project setup

---
+++ src/main/java/com/goanna/CapstoneProjectTwoApplication.java
@@ -0,0 +1,13 @@
+package com.goanna;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CapstoneProjectTwoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CapstoneProjectTwoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/goanna/CapstoneProjectTwoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.goanna;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CapstoneProjectTwoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
